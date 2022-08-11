e6674dd75c1baf8d4908091d91bb3f0750238189
Piper McCorkle <contact@piperswe.me>
2022-08-11 16:50:08 -0500

    Add description to MaroonTag

--- src/lib/components/MaroonTag.svelte
+++ src/lib/components/MaroonTag.svelte
@@ -1,3 +1,8 @@
+<!--
+	This component is a Carbon tag which uses A&M's maroon colors.
+	Colors are taken from https://brandguide.tamu.edu/web/web-color-palette.html.
+-->
+
 <div class="bx--tag maroon-tag"><span><slot /></span></div>
 
 <style>
